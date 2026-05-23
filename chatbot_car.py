dd7fd864614daef1c15ff0742f2d40dd14b58684
Angelica <angelicacaroline1995@gmail.com>
2026-05-23 01:28:16 -0300

    🚧 wip: chatbot rodando no navegador com streamlit

--- chatbot_car.py
+++ chatbot_car.py
@@ -11,13 +11,17 @@ client = Groq(api_key=config["api_key"])
 st.set_page_config(page_title="Carbot")
 st.title("Carbot friend")
 
-chat = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Olá, me ajude a escolher um carro. Eu gosto de carros velozes",
-        }
-    ],
-    model="llama-3.3-70b-versatile",
-)
-print(chat.choices[0].message.content)
+input = st.text_input("Digite sua pergunta sobre carros:")
+
+if st.button("Enviar"):
+    if input:
+        with st.spinner("Vrum vrumm..."):
+            chat = client.chat.completions.create(
+            messages=[{"role": "user", "content": input}],
+            model="llama-3.3-70b-versatile",
+            )
+            resposta = chat.choices[0].message.content
+            st.write("Resposta do Carbot:")
+            st.info(resposta)
+    else:
+        st.warning("Por favor, me diga qual é a sua dúvida?")
